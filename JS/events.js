4e6c7fe3430cf3935f02aef5f16a81966a661fed
Najam-Unnisa <najamunnisa00@gmail.com>
2025-07-04 11:18:37 +0530

    events

---
+++ JS/events.js
@@ -0,0 +1,22 @@
+
+let Btn = document.querySelector("#show").addEventListener("click", ()=>
+{
+   
+   const input = document.querySelector("#pass")
+    if(input.getAttribute("type")=="password"){
+input.setAttribute("type","text")
+document.querySelector("#show").textContent="hide"
+document.querySelector("#show").style.backgroundColor="red"
+const a =document.getElementById("h1")
+
+// setTimeout(()=>{
+//    a.textContent="Button was clicked!!!"
+    
+// },3000)
+    }else{
+        input.setAttribute("type","password")
+        document.querySelector("#show").textContent="show"
+    }
+})
+
+
